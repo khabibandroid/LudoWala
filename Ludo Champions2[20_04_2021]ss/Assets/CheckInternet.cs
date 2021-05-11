220f3a3436a89c69662260c5541b922c13b5f8c3
PradeepHGK <pradeep.hgk@ziniosedge.com>
2021-05-11 21:33:17 +0530

    project added

---
+++ Ludo Champions2[20_04_2021]ss/Assets/CheckInternet.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckInternet : MonoBehaviour
+{
+    public GameObject blocker;
+    void Awake()
+    {
+        if (!GameManager.Instance.offlineMode && GameManager.Instance.type != MyGameType.comp)
+            StartCoroutine("CheckConnection");
+    }
+
+    IEnumerator CheckConnection()
+    {
+        while (true)
+        {
+            blocker.SetActive(Application.internetReachability == NetworkReachability.NotReachable);
+            if (EventCounter.GetID() == "") EventCounter.SetID(GameManager.Instance.UserID);
+            yield return new WaitForSeconds(1f);
+        }
+    }
+
+}
